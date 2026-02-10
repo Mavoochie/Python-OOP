010dc90b139838f22d3f09b2e979049fe7740ec6
Marvin <marvinbernard2018@gmail.com>
2026-02-10 09:32:35 +0300

    OOP exercise

--- oop-1.py
+++ oop-1.py
@@ -13,3 +13,16 @@ car2 = Car("Mazda","CX-7")
 car1.drive()
 car2.drive()
 
+# Exercise
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        print(f"My name is {self.name} and I am {self.age} years old")
+s1 = Student("Alice", 20)
+s2 = Student("Bob", 22)
+
+s1.introduce()
+s2.introduce()
